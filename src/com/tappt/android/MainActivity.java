2f147933861a728905771308216de89386295637
John Kalberer <jlkalberer@gmail.com>
2013-04-28 18:54:30 -0700

    Updated to newer SDK.  Starting to build out application since NFC is now working with Arduino.

--- src/com/tappt/android/MainActivity.java
+++ src/com/tappt/android/MainActivity.java
@@ -1,75 +1,41 @@
 package com.tappt.android;
 
-import com.tappt.android.R.id;
-
-import android.os.Bundle;
 import android.app.Activity;
-import android.app.AlertDialog;
-import android.app.AlertDialog.Builder;
-import android.app.Dialog;
-import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
 import android.view.Menu;
-import android.view.View;
-import android.widget.EditText;
-import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
-	private EditText emailAddress;
-	private EditText password;
+	public static final String PREFS_NAME = "Tappt";
 	
-	private TapptRestClient client;
+	private static final String PREF_USERNAME = "username";
+	private static final String PREF_PASSWORD = "password";
 	
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        
-        this.emailAddress = (EditText)this.findViewById(id.email);
-        this.password = (EditText)this.findViewById(id.password);
-        this.client = new TapptRestClient();
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		CheckLogin();
+	}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-    
-    
-    /**
-     * The click event fired when you want to login.
-     * 
-     * @param view
-     */
-    public void OnLoginClick(View view)
-    {
-    	String emailAddress = this.emailAddress.getText().toString();
-    	if (emailAddress == null || emailAddress.isEmpty()) {
-    		this.ShowError("Email must not be empty.");
-    		return;
-    	}
-    	
-    	String password = this.password.getText().toString();
-    	if (password == null || password.isEmpty()) {
-    		this.ShowError("Password must not be empty.");
-    		return;
-    	}
-    	
-    	this.client.Authenticate(emailAddress, password, true, null);
-    }
-    
-    private void ShowError(String error) {
-    	AlertDialog.Builder builder = new AlertDialog.Builder(this);
-    	builder.setMessage(error)
-    	       .setCancelable(false)
-    	       .setPositiveButton("OK", new DialogInterface.OnClickListener() {
-    	           public void onClick(DialogInterface dialog, int id) {
-    	                dialog.dismiss();
-    	           }
-    	       });
-    	AlertDialog alert = builder.create();
-    	alert.show();
-    }
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	private void CheckLogin(){ 
+		SharedPreferences preferences = getPreferences(MODE_PRIVATE);
+		
+		Intent login = new Intent(this, LoginActivity.class);
+		if (preferences == null) {
+			startActivity(login);
+		}
+		
+		startActivity(login);
+	}
 }
